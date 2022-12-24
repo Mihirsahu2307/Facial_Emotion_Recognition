e75b62e6f38dec8b34d4940497e1f509ad58a3fc
Mihirsahu2307 <74849707+Mihirsahu2307@users.noreply.github.com>
2022-12-24 17:53:08 +0530

    Rename mobilenet_model.py to prediction_model.py

---
+++ prediction_model.py
@@ -0,0 +1,24 @@
+import tensorflow as tf
+from keras import Sequential
+from keras.layers import Flatten, BatchNormalization, Dense
+import os
+
+
+# model6 using the mobile_net as the base model, trained on Kaggle. Input = (224, 224)
+# Currently, mobilenet based model is used, to use the Resnet50 based model, make appropriate changes by referring to the jupyter notebook
+
+classes_num = 7
+img_size = 224
+Project_DIR = "C:\\Users\\USER\\Desktop\\ML-Projects\\Face_Emotion_Recognition"
+mobilenet_model = tf.keras.applications.MobileNetV2(input_shape=(img_size, img_size, 3), include_top=False, weights='imagenet')
+
+model6 = Sequential()
+model6.add(mobilenet_model)
+
+model6.add(Flatten())
+model6.add(BatchNormalization())
+
+model6.add(Dense(classes_num, activation='softmax'))
+
+# model6.summary()
+model6.load_weights(os.path.join(Project_DIR, "model_mobilenet", "model_mobilenet_input_224_50_epochs.h5"))
